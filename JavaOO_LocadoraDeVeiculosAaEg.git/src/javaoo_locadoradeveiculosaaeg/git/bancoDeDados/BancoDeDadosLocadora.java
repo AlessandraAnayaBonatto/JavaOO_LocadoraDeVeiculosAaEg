a03987d9b4624cf0d9a72c00e5d6f0973521b74d
Eduardo Guedes <eduardo.s.guedes@hotmail.com>
2016-11-30 21:01:49 -0200

    Inicio de envio do projeto ao Git Hub, já criados: models/ClientePessoaFisicaModel.java models/ClientePessoaJuridicaModel.java models/PessoaFisica.java models/PessoaJuridica.java bancoDeDados/BancoDeDadosLocadora.java

---
+++ JavaOO_LocadoraDeVeiculosAaEg.git/src/javaoo_locadoradeveiculosaaeg/git/bancoDeDados/BancoDeDadosLocadora.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoo_locadoradeveiculosaaeg.git.bancoDeDados;
+
+/**
+ *
+ * @author eduar
+ */
+public class BancoDeDadosLocadora {
+    
+}
---
+++ JavaOO_LocadoraDeVeiculosAaEg.git/src/javaoo_locadoradeveiculosaaeg/git/models/ClientePessoaJuridicaModel.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoo_locadoradeveiculosaaeg.git.models;
+
+/**
+ Classe utilizada para cadastro de clientes pessoa juridica no sistema;
+ */
+public class ClientePessoaJuridicaModel {
+    private String cep;
+    private String endereco;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
