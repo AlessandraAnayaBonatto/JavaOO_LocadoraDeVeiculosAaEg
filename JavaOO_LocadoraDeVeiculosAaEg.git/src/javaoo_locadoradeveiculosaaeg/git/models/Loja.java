845e95169ce7ef51217673d446bac58f51ef69ee
Philip Gonçalves Pencal <gpencal@hotmail.com>
2016-12-04 22:05:14 -0200

    Criação da classe carro e da classe loja criação dos includes de carro, loja, pessoa fisica e pessoa juridica e cartao de credito Alessandra

---
+++ JavaOO_LocadoraDeVeiculosAaEg.git/src/javaoo_locadoradeveiculosaaeg/git/models/Loja.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoo_locadoradeveiculosaaeg.git.models;
+
+/**
+ *
+ * @author Philip
+ */
+public class Loja {
+    
+    private String nome;
+    private String endereco;
+    private String horarios;
+    private float valores;
+    private String locaisDisponiveis;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the horarios
+     */
+    public String getHorarios() {
+        return horarios;
+    }
+
+    /**
+     * @param horarios the horarios to set
+     */
+    public void setHorarios(String horarios) {
+        this.horarios = horarios;
+    }
+
+    /**
+     * @return the valores
+     */
+    public float getValores() {
+        return valores;
+    }
+
+    /**
+     * @param valores the valores to set
+     */
+    public void setValores(float valores) {
+        this.valores = valores;
+    }
+
+    /**
+     * @return the locaisDisponiveis
+     */
+    public String getLocaisDisponiveis() {
+        return locaisDisponiveis;
+    }
+
+    /**
+     * @param locaisDisponiveis the locaisDisponiveis to set
+     */
+    public void setLocaisDisponiveis(String locaisDisponiveis) {
+        this.locaisDisponiveis = locaisDisponiveis;
+    }
+    
+ 
+    
+    
+}
