44f59dfcf15de6ebffd8bfaceb925bef3c7c0faa
Alessandra Anaya Bonatto <alessandra_anaya@hotmail.com>
2016-12-08 01:22:24 -0200

    Finalização dos creates em todas os controllers Criação do delete de todos os controllers Criação de todas as views Atualização do MenuView chamando todos os métodos Criação das classe para Locação (Precisamos definir o que terá nela). Alessandra

--- JavaOO_LocadoraDeVeiculosAaEg.git/src/javaoo_locadoradeveiculosaaeg/git/models/LocacaoModel.java
+++ JavaOO_LocadoraDeVeiculosAaEg.git/src/javaoo_locadoradeveiculosaaeg/git/models/LocacaoModel.java
@@ -10,6 +10,23 @@ package javaoo_locadoradeveiculosaaeg.git.models;
  * @author aless
  */
 public class LocacaoModel {
+
+    /**
+     * @return the teste
+     */
+    public int getTeste() {
+        return teste;
+    }
+
+    /**
+     * @param teste the teste to set
+     */
+    public void setTeste(int teste) {
+        this.teste = teste;
+    }
+    
+    private int teste;
+    
     
     
     
